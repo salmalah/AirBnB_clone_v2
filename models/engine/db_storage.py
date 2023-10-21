67635b2ab714ef3bc1fa5d3558a4af7649eb3ac3
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-21 21:57:55 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,14 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base, BaseModel
 import os
-from models.user import User
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.review import Review
-from models.base_model import BaseModel
-from models.place import Place
-classes = [User, State, City, Amenity, Place, Review]
 
 
 class DBStorage:
@@ -23,61 +15,72 @@ class DBStorage:
 
     def __init__(self):
         """Public instance method"""
-        user = os.getenv('HBNB_MYSQL_USER')
-        pwd = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine(
-                "mysql+mysqldb://{}:{}@{}/{}".format(user, pwd, host, db),
-                pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
+                                      format(os.getenv('HBNB_MYSQL_USER'),
+                                             os.getenv('HBNB_MYSQL_PWD'),
+                                             os.getenv('HBNB_MYSQL_HOST'),
+                                             os.getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        with self.session_scope() as session:
-            if cls:
-                return session.query(cls).all()
-            else:
-                return session.query(User, State, City, Amenity, Place, Review).all()
+        """
+        Method to retrieve all objects"""
+        from models.user import User
+        from models.city import City
+        from models.state import State
+        from models.amenity import Amenity
+        from models.review import Review
+        from models.base_model import BaseModel
+        from models.place import Place
+
+        clas = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review}
+        if cls:
+            new_d = {}
+            allC_obj = self.__session.query(cls).all()
+            for o in allC_obj:
+                k = type(o).__name__ + "." + o.id
+                new_d[key] = o
+            return (new_d)
+        else:
+            all_d = {}
+            dic_l = []
+            State = self.all('State')
+            City = self.all('City')
+            dic_l.append(State)
+            dic_l.append(City)
+            for d in dic_l:
+                all_d.update(d)
+            return(all_d)
 
     def new(self, obj):
-        with self.session_scope() as session:
-            session.add(obj)
+        """add ibjet to db"""
+        self.__session.add(obj)
 
     def save(self):
-        with self.session_scope() as session:
-            session.commit()
+        """
+        commit change of database"""
+        self.__session.commit()
 
     def delete(self, obj=None):
-        with self.session_scope() as session:
-            if obj:
-                session.delete(obj)
+        """method the delete from db session"""
+        if obj:
+            self.__session.delete(obj)
 
     def reload(self):
+        """
+        reloandig database"""
         Base.metadata.create_all(self.__engine)
-        ss_f = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        ss_f = sessionmaker(bind=self.__engine,
+                            expire_on_commit=False)
         Session = scoped_session(ss_f)
         self.__session = Session()
 
-    @contextmanager
-    def session_scope(self):
-        session = self.get_session()
-        try:
-            yield session
-            session.commit()
-        except Exception as e:
-            session.rollback()
-            raise e
-        finally:
-            session.close()
-
-    def get_session(self):
-        if self.__session is None:
-            self.reload()
-        return self.__session
-
     def close(self):
-        if self.__session:
-            self.__session.close()
-
+        """
+        Method close session"""
+        self.__session.close()
 
