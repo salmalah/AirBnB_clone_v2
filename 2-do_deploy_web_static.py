0ad18ce1e4a4f94ad7d79f311bbb537db314a07f
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-22 03:08:09 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,21 +15,18 @@ def do_pack():
     """
     Pack web_static into .tgz archive
     """
-
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_path = "versions/web_static_{}.tgz".format(date)
 
-    if local("tar -cvzf {} web_static".format(archive_path)).succeeded:
+    if local("tar -czvf {} web_static".format(archive_path)).succeeded:
         return archive_path
     return None
-
 
 def do_deploy(archive_path):
     """
     Deploy an archive to web servers
     """
-
     if not os.path.exists(archive_path):
         return False
 
@@ -40,15 +37,12 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(release_dir))
         run("sudo tar -xzf /tmp/{} -C {}".format(archive_name, release_dir))
-
         run("sudo rm /tmp/{}".format(archive_name))
 
         run("sudo mv {}/web_static/* {}/".format(release_dir, release_dir))
-
         run("sudo rm -rf {}/web_static".format(release_dir))
 
         run("sudo rm -rf /data/web_static/current")
-
         run("sudo ln -s {} /data/web_static/current".format(release_dir))
 
         print("New version deployed!")
