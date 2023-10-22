516a53a9e69c759988f7f912b322861265706af3
Salmalah <s.salmalahmidi@gmail.com>
2023-10-21 18:53:18 -0700

    fix errors style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo.
+Fabric script that generates a .tgz archive
 """
 
 from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """
-    Generates a .tgz archive from the contents of the web_static folder.
-    Returns the archive path if successful, or None if there's an error.
+    Generates a .tgz archive from the contents of the web_static
     """
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     archive_name = "versions/web_static_{}.tgz".format(timestamp)
@@ -23,4 +23,3 @@ def do_pack():
         return archive_name
     except Exception:
         return None
-
